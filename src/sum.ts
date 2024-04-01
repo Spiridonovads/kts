ced86caceeac921fb0a2ce6e0da7ee738d3b8b56
Divnshv <spiridonova_2804a@mail.ru>
2024-04-01 21:55:27 +0300

    sum function

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,20 @@
-const sum = () => {};
+const sum = (...args) => {
+  let res = 0;
+  args.forEach((el) => {
+    if (typeof el !== 'number') {
+      throw new Error('INVALID_ARGUMENT');
+    }
+  });
 
-export default sum;
+  if (args.length <= 1) {
+    throw new Error('INVALID_ARGUMENTS_COUNT');
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    res += args[i];
+  }
+
+  return res;
+};
+
+export default sum
