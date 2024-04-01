07c670c3bc36b8eb1cb28695b523099fef591210
Divnshv <spiridonova_2804a@mail.ru>
2024-04-02 00:19:18 +0300

    pow

--- src/pow.ts
+++ src/pow.ts
@@ -1,3 +1,21 @@
-const pow = () => {};
+const pow = (...arg) => {
+  if (typeof arg[0] !== 'number') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  const degree = (c) => {
+    if (typeof c !== 'number') {
+      throw new Error('INVALID_ARGUMENT');
+    }
+    return arg[0] ** c;
+  };
+  if (arg.length > 1) {
+    if (typeof arg[1] !== 'number') {
+      throw new Error('INVALID_ARGUMENT');
+    }
+    return arg[0] ** arg[1];
+  } else {
+    return degree;
+  }
+};
 
 export default pow;
