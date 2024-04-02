0f48748298f89fa23ec180aae8afa886304505d4
Divnshv <spiridonova_2804a@mail.ru>
2024-04-02 22:32:38 +0300

    getNumberProps

--- src/getNumberProps.ts
+++ src/getNumberProps.ts
@@ -1,3 +1,24 @@
-const getNumberProps = () => {};
+const getNumberProps = (obj) => {
+  let ans:string[] = [];
+  if (typeof obj !== 'object' || Array.isArray(obj) || obj === null) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+	myFn(obj);
+
+  function myFn (obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'object') {
+        myFn(obj[key]);
+      } else {
+        if (typeof obj[key] === 'number') {
+          ans.push(key);
+        }
+      }
+    }
+  };
+
+  return ans.sort()
+};
 
 export default getNumberProps;
