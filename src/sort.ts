714be134ee3e758e8a451693f9e89306554bda0f
Divnshv <spiridonova_2804a@mail.ru>
2024-04-02 15:49:37 +0300

    sort

--- src/sort.ts
+++ src/sort.ts
@@ -1,3 +1,30 @@
-const sort = () => {};
+const sort = (a) => {
+	if(typeof a !== 'string') {
+		throw new Error('INVALID_ARGUMENT')
+	}
+	let ans = ''
+	let res = a.split(' ').sort((b, c) => {
+		return b.length - c.length
+	}).map(el => {
+		return el.split('').map(letter => {
+			if(letter.toUpperCase() === letter){
+				letter = letter.toLowerCase()
+			}
+			return letter
+		}).join('')
+	}).flat().map(el => {
+		return el.split('').sort().join('')
+	})
+
+	res.forEach((el, i) => {
+		if(i !== res.length - 1) {
+			ans += el + ' '
+		} else {
+			ans += el
+		}
+		
+	})
+	return ans
+};
 
 export default sort;
