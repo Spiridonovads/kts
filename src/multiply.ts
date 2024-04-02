68f0cbdf9bd343369709321468ce2862e777ee82
Divnshv <spiridonova_2804a@mail.ru>
2024-04-02 21:30:35 +0300

    multiply

--- src/multiply.ts
+++ src/multiply.ts
@@ -1,3 +1,14 @@
-const multiply = () => {};
+const multiply = (a) => {
+	if(typeof a !== 'number'){
+		throw new Error('INVALID_ARGUMENT')
+	}
+	const myFn = (b) => {
+		if(typeof b !== 'number'){
+			throw new Error('INVALID_ARGUMENT')
+		}
+		return a * b
+	}
+	return myFn
+};
 
 export default multiply;
