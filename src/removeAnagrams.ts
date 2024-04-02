93c7ed754d33fff28378b92a1771811da9e24e76
Divnshv <spiridonova_2804a@mail.ru>
2024-04-02 18:53:53 +0300

    removeAnagrams

--- src/removeAnagrams.ts
+++ src/removeAnagrams.ts
@@ -1,3 +1,34 @@
-const removeAnagrams = () => {};
+const removeAnagrams = (arr) => {
+  if (!Array.isArray(arr)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  arr.forEach((el) => {
+    if (typeof el !== 'string') {
+      throw new Error('INVALID_ELEMENT_IN_ARRAY');
+    }
+  });
+  let res = {};
+  let ans: string[] = [];
+  arr.forEach((el) => {
+    el = el.split('').sort().join('');
+    if (!res[el]) {
+      res[el] = 1;
+    } else {
+      res[el] += 1;
+    }
+  });
+
+  for (let key in res) {
+    if (res[key] === 1) {
+      arr.forEach((el) => {
+				if(el.split('').sort().join('') === key){
+					ans.push(el)
+				}
+			});
+    }
+  }
+
+  return ans
+};
 
 export default removeAnagrams;
