e08a632ef5b40eb31f3bcc7fc96c47d3e46279e0
Divnshv <spiridonova_2804a@mail.ru>
2024-04-03 13:57:13 +0300

    dfs

--- src/dfs.ts
+++ src/dfs.ts
@@ -1,3 +1,28 @@
-const dfs = () => {};
+const dfs = (graph) => {
+  if (typeof graph !== 'object' || Array.isArray(graph) || graph === null) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  let arr: string[] = [];
+
+  for (let key in graph) {
+    arr.push(key);
+    myFn(graph[key]);
+    break;
+  }
+  function myFn(value) {
+    value.forEach((el) => {
+      for (let key in graph) {
+        if (el === key) {
+          arr.push(key);
+          if (graph[key].length > 0) {
+            myFn(graph[key]);
+          }
+        }
+      }
+    });
+  }
+
+  return arr;
+};
 
 export default dfs;
