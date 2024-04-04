f410bee92382416c6573b53771054b1928f2fa73
Divnshv <spiridonova_2804a@mail.ru>
2024-04-04 19:33:33 +0300

    promiseFrame

--- src/promiseFrame.ts
+++ src/promiseFrame.ts
@@ -8,14 +8,53 @@ type FunctionResultsUnion<
   [K in keyof T]: Awaited<ReturnType<T[K]>>;
 }[number];
 
-const promiseFrame = async <
+async function promiseFrame<
   T extends FunctionType<any>[] | Readonly<FunctionType<any>[]>,
   ResultsT = FunctionResultsUnion<T>
->(
-  functions: T,
-  limit?: number
-): Promise<ResultsT[]> => {
-  return [];
-};
+>(functions: T, limit?: number): Promise<ResultsT[]> {
+  if (!Array.isArray(functions)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  if (typeof limit !== 'undefined' && typeof limit !== 'number') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  if (typeof limit === 'number' && limit <= 0) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  if (typeof limit === 'undefined') {
+    return Promise.all(functions.map((func) => func()));
+  }
+
+  let activePromises = 0;
+  let currentIndex = 0;
+  const results: any[] = new Array(functions.length);
+
+  return new Promise((resolve, reject) => {
+    const executeNext = () => {
+      if (currentIndex === functions.length && activePromises === 0) {
+        return resolve(results);
+      }
+      while (activePromises < limit! && currentIndex < functions.length) {
+        const index = currentIndex;
+        activePromises++;
+        Promise.resolve(functions[index]())
+          .then((result) => {
+            results[index] = result;
+            activePromises--;
+            executeNext();
+          })
+          .catch((error) => {
+            reject(error);
+          });
+
+        currentIndex++;
+      }
+    };
+    executeNext();
+  });
+}
 
 export default promiseFrame;
