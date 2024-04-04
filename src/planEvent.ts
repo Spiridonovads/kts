71946542aed095755ceb1b1ac10eaeaf7043aa0f
Divnshv <spiridonova_2804a@mail.ru>
2024-04-04 17:03:41 +0300

    planEvent

--- src/planEvent.ts
+++ src/planEvent.ts
@@ -1,3 +1,14 @@
-const planEvent = () => {};
+function planEvent(f, timeout) {
+  if (typeof f !== 'function') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  if (typeof timeout !== 'number') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  let promise = new Promise((resolve) => {
+    setTimeout(() => resolve(f()), timeout);
+  });
+  return promise.then((res) => res);
+}
 
 export default planEvent;
